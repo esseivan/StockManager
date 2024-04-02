57879346ed939dcf2a0905d0ad821b14beeb2390
EsseivaN <nicolas.esseiva@gmail.com>
2024-04-02 12:45:33 +0200

    frmMain astyle formatting

--- StockManagerDB/astyleFormat.sh
+++ StockManagerDB/astyleFormat.sh
@@ -1,2 +1,2 @@
-astyle --style=allman --add-one-line-braces --max-code-length=80 --break-after-logical --indent-switches --convert-tabs --indent=spaces=4 --recursive --pad-header --pad-comma --pad-oper --max-continuation-indent=80 --attach-closing-while --align-pointer=type $1
+"C:\Workspace\Github\StockManager\StockManagerDB\AStyle.exe" --style=allman --add-one-line-braces --max-code-length=80 --break-after-logical --indent-switches --convert-tabs --indent=spaces=4 --pad-header --pad-comma --pad-oper --max-continuation-indent=80 --attach-closing-while --align-pointer=type $1
 
